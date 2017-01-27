0196881e72836a276b29d7ca3716318d1f71ea4d
Pracser Tamas <t.pracser@gmail.com>
2017-01-27 01:52:48 +0100

    keller in tha house

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var button = document.querySelector('button');
 var select = document.querySelector('#select');
 var ul = document.querySelector('#result');
 var number = document.querySelector("#members");
+var firstText = document.querySelector(".firsttext");
+var secondText = document.querySelector(".secondtext");
 
 button.addEventListener('click', function () {
   ul.innerText = "";
@@ -11,17 +13,25 @@ button.addEventListener('click', function () {
        var gender = document.createElement('div');
         gender.setAttribute('class','genderType');
     for(var k = 0; k < number.value; k++){
-      var li = document.createElement('li');
-        for(var i = 0; i < number.value; i++){
-          var select = document.createElement('select');
-          for(var j = 0; j < number.value; j++){
-            var option = document.createElement('option');
-            option.setAttribute('value','korte');
-            option.innerText = "option" + (j+1);
-            select.appendChild(option);
+        var li = document.createElement('li');
+        var inputfield = document.createElement('input');
+        inputfield.setAttribute("class", "tableheaders");
+        if(c==0){
+            inputfield.value =  firstText.value + " " + (k+1);
+        } else {
+            inputfield.value =  secondText.value + " " + (k+1);
         }
-          li.appendChild(select)
-      }
+        li.appendChild(inputfield)
+        for(var i = 0; i < number.value; i++){
+            var select = document.createElement('select');
+            for(var j = 0; j < number.value; j++){
+                var option = document.createElement('option');
+                option.setAttribute('value','korte');
+                option.innerText = "option" + (j+1);
+                select.appendChild(option);
+            }
+            li.appendChild(select)
+        }
       ul.appendChild(li)
     }
     li.appendChild(gender)
